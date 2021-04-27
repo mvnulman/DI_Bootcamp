1305d09ea2919955ddb7fa3172e9d87f9c1a962e
Marcos Vinicius <mvsouza7@gmail.com>
2021-04-27 15:38:50 +0300

    xp-challenges update

--- Week 4/Day 2/xp-challenges/script.js
+++ Week 4/Day 2/xp-challenges/script.js
@@ -172,14 +172,50 @@
 // Exercise 6 : What’s In My Wallet ?
 //------------------------------------
 
+// Exercise 6 : What’s In My Wallet ?
+// Instructions
+// Given a item price and an array representing the amount of change in your pocket, determine whether or not you can afford the item.
+// Change will always be represented in the following order: quarters, dimes, nickels, pennies.
+// Quarters  = 0.25
+// Dimes = 0.10
+// Nickels = 0.05
+// Pennies = 0.01
+// To illustrate:
+// changeEnough([25, 20, 5, 0], 4.25) should return true, 
+// since having 25 quarters, 
+// 20 dimes, 
+// 5 nickels 
+// and 0 pennies 
+// gives you 6.25 + 2 + .25 + 0 = 8.50 which is bigger than 4.25 (the total amount due)
 
+// Examples
 
+// changeEnough([2, 100, 0, 0], 14.11) ➞ false
+// changeEnough([0, 0, 20, 5], 0.75) ➞ true
 
+// function changeEnough(arr,comparator){
+// 	let totalAmount = 0,currentValue
+// 	arr.forEach((element,index) => {
+// 		switch (index) {
+// 			case 0:
+// 				currentValue = 0.25
+// 			break;
+// 			case 1:
+// 				currentValue = 0.10
+// 			break;
+// 			case 2:
+// 				currentValue = 0.05
+// 			break;
+// 			default:
+// 				currentValue = 0.01
+// 			break;
+// 		}
+// 		totalAmount += element&currentValue
 
+// 	});
 
-
-
-
+// 	return comparator-currentValue>0
+// }
 
 
 //-----------------------------
@@ -232,3 +268,6 @@
     // }
 
     // myBill();
+
+
+    
