3aee28ab16dab09debba036adf4846f3a5ad31f3
Marcos Vinicius <mvsouza7@gmail.com>
2021-05-01 20:52:50 +0300

    mini-project calculator. added html, css and js files

---
+++ Week 4/Day 3/mini-project/calculator/script.js
@@ -0,0 +1,17 @@
+let display = document.getElementById('display');
+
+let buttons = Array.from(document.getElementsByClassName('button'));
+
+buttons.map( button => {
+    button.addEventListener('click', (e) => {
+        switch(e.target.innerText){
+            case 'C':
+                display.innerText = '';
+                break;
+            case '←':
+                display.innerText = display.innerText.slice(0, -1);
+            default:
+                display.innerText += e.target.innerText;
+        }
+    });
+});
