68a31e3e3a4a8fe1e1dcfb77585d7ed5a8990c89
Marcos Vinicius <mvsouza7@gmail.com>
2021-05-23 17:56:33 +0300

    Week 3, day 2 - xp ninja

---
+++ Week 3/Day 2/xp-ninja/script.js
@@ -0,0 +1,101 @@
+//------------------------
+// Exercise 1 : Find Nemo
+//------------------------
+// Instructions
+// Ask the user to give you a sentence containing the word “Nemo”. For example "I love the movie named Nemo !"
+// Find the word “Nemo”
+// Console.log a string as follows: "I found Nemo at [the position of the word Nemo]!".
+// Bonus : If you can’t find Nemo, console.log “I can’t find Nemo”.
+// Hint : use an if/else statement
+
+// Some examples:
+
+//     "I love the movie named Nemo !" ➞ "I found Nemo at 5"
+//     "Nemo is a cute fish" ➞ "I found Nemo at 0"
+//     "My fish is called Nemo, I love it" ➞ "I found Nemo at 4"
+
+
+
+// let userSentence = prompt("please say a sentence with nemo word")
+
+// let nemoPosition  = userSentence.toLowerCase().split(" ").indexOf("nemo")
+
+// if(nemoPosition!=-1){
+//     alert(`I found Nemo at ${nemoPosition}!`)
+// }else{
+//     alert(`I can’t find Nemo`)
+// }
+
+//------------------------
+// Exercise 1 : Evaluation
+//------------------------
+// Instructions
+// Evaluate the comparisons found below:
+// Look at this link for help
+// http://sticksandstones.kstrom.com/appen.html
+
+ // Exercise 1 : Evaluation
+
+// 5 >= 1
+// output true
+// console.log(5 >= 1)
+// 0 === 1
+// output false
+// console.log(0 === 1)
+// 4 <= 1
+// output false
+// console.log(4 <= 1)
+// 1 != 1
+// output false
+// console.log(1 != 1)
+// "A" > "B"
+// output false
+//  console.log( "A"> "B")
+// "B" < "C"
+// output true
+//  console.log( "B"< "C")
+// "a" > "A"
+// output true
+//  console.log( "a"> "A")
+// "b" < "A"
+// output false
+//  console.log("b"< "A")
+// true === false
+// output false
+// console.log(true === false)
+// true != true
+// output false
+// console.log(true != true)
+
+//------------------------
+// Exercise 2 : Evaluation
+//------------------------
+// Instructions
+// Evaluate the code below. what would be the outcome if you run the code in the Javascript Console.
+// Answer the questions below then check them line by line in the console.
+
+// let c;
+// let a = 34;
+// let b = 21;
+// a = 2;
+// a + b
+
+// 1 - What will be the outcome of a + b?
+// Result: a + b = 23
+
+// 2 - What is the value of c?
+// Result: c = undefined.
+// console.log(c);
+
+// 3 - Analyse the code below what will be the outcome?
+// console.log(3 + 4 + '5');
+//Result: 75
+
+//------------------------------
+// Exercise 3 : Ask for Numbers
+//-----------------------------
+// Instructions
+// Ask the user for a string of numbers separated by commas, console.log the sum of the numbers.
+// Hint: use some string methods
+
+
