328bdd1853b583653d9134aa42a55323880ff0f2
Marcos Vinicius <mvsouza7@gmail.com>
2021-07-07 20:51:12 +0300

    Exercises update

--- Week 14/Day 1/react/jswebreact/src/Hello.js
+++ Week 14/Day 1/react/jswebreact/src/Hello.js
@@ -1,16 +1,16 @@
-import './Hello.css'
-import Card from 'react-bootstrap/Card';
-import Col from 'react-bootstrap/Col';
+// import Card from 'react-bootstrap/Card';
+// import Col from 'react-bootstrap/Col';
 
-const Hello = ({user}) => {
+const Robots = ({user}) => {
   // const {user} = props;
-  const {name, email, username} = user;
+  const {name, email, username,id} = user;
   return (
     <div className='tc grow bg-light-green br3 pa3 ma2 dib bw2 shadow-5'>
-      <h3>{name}</h3>
-      <h4>{email}</h4>
+      <img src={`https://robohash.org/${id}?size=200x200`} />
+      <h4>{name}</h4>
+      <p>{email}</p>
       <p>{username}</p>
     </div>
   )
 }
-export default Hello;
+export default Robots;
